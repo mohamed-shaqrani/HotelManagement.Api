919053b3a6882dc64f79f37e334d6be0145ac268
Mohamed Shaqrani <mohamedshaqrani@gmail.com>
2025-01-06 07:17:38 +0200

    add reservation and payment entity

---
+++ HotelManagement/Controllers/CheckoutController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelManagement.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class CheckoutController : ControllerBase
+{
+}
